e35027756886441f498f51f164630bb6501f65dd
Tym P <tim.pakulski@gmail.com>
2015-03-26 15:45:57 +0100

    updated pDropAuto to calculate inlet vapour quality and notify user.

--- pressureDropScripts/pDropAuto.m
+++ pressureDropScripts/pDropAuto.m
@@ -1,9 +1,12 @@
 %Pressure drop calculation
-%Tym Pakulski
-%25 March 2015
+%%%Tym Pakulski
+%%%25 March 2015
+%%%Calculates the pressure drop through a straight pipe given inlet
+%%%conditions of temperature, pressure and mass flow rate. Implements the
+%%%Colebrook equation to solve for the Darcy friction factor. 
 %INPUTS***************************************************************
 %Flow Characteristics
-T=-20; %C
+T=-18; %C
 P = 20; %bar
 m=.125; %kg/s
 
@@ -16,11 +19,14 @@ R = .015*10^(-3); % m absolute roughness
 %Unit Conversions to REFPROP
 P=P*100;%kPa
 T=T+273;%K
-relR=R/d
+relR=R/d;
 
 %Determining Density
 cd('../REFPROP'); %Change path to location of REFPROP files.
 D=refpropm('D','T',T,'P',P,'CO2');%kg/m^3
+
+%Determine inlet vapour quality
+Qin=refpropm('Q','T',T,'P',P,'CO2');
 
 %Determining Viscosity
 %mu=145.1*10^(-6);%Pas
@@ -51,16 +57,30 @@ H4=refpropm('H','T',T,'P',P,'CO2');
 %Vapor quality at 4*
 Q4=refpropm('Q','H',H4,'P',newP,'CO2');
 
+%Switch back to original directory.
+cd('../matlabScripts')
 %User info:
+
+disp('INLET CONDITIONS*********************************');
+if(Qin>0)
+    display('Flow is 2-phase at inlet.')
+    display(Qin)
+else
+    display('Flow is liquid at inlet.')
+end
 display(['Reynolds number = ', num2str(Re)])
 if(Re>2040)
     disp('Flow is turbulent.')
 end
+
+disp(' ');
+disp(' ');
+disp('DISCHARGE CONDITIONS*****************************');
 display(['Calculated friction factor = ', num2str(Df)])
 display(['Pressure drop = ', num2str(dP/10^5), ' bar'])
 if(Q4>0)
     display('Flow is 2-phase after pressure drop.')
-    display(Q4)
+    display(['Vapour Quality = ',num2str(Q4)]);
 else
     display('Flow is liquid after pressure drop.')
 end
